9c81bb9a05624a62da11a75ecb008c276cfd74e9
Juan Hoyos <juanhoyos1347@gmail.com>
2024-03-28 14:29:57 -0500

    fix create investment

--- src/controllers/investment.controller.js
+++ src/controllers/investment.controller.js
@@ -1,14 +1,25 @@
 const investmentService = require('../services/investment.service');
 const createInvestment = async (req, res) => {
-    const { description, quantity, price, individual_total, total, type_of_investment, user_id } = req.body;
+    const { description, quantity,price,individual_total,total,type_of_investment,user_id } = req.body;
     const newInvestment = {
         description, quantity, price, individual_total, total, type_of_investment, user_id
-    }
+    }   
     try {
         const response = await investmentService.createInvestment(newInvestment);
-        return response;
+        if (response?.errors) {
+            res.status(400).json({
+                status:'BAD REQUEST',
+                data: "Ups, vuelve a intentarlo",
+            })
+        }
+        res.status(201).json({
+            msg: "Investment created",
+        })
     } catch (error) {
-        return error;
+        res.status(500).json({
+            msg: 'Error en el servidor',
+            error: error
+        })
     }
 }
 
